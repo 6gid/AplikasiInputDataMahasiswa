b99db6cb5a18c2d0243520891d0e5138f83c8625
6gid <119929188+6gid@users.noreply.github.com>
2024-11-09 10:40:14 +0700

    Add files via upload
    #AplikasiInputDataMahasiswa
    **AplikasiInputDataMahasiswa** adalah sebuah aplikasi yang dirancang untuk memudahkan pengelolaan data mahasiswa di lingkungan perguruan tinggi. Aplikasi ini memungkinkan pengguna (staf administrasi atau dosen) untuk memasukkan, memperbarui, dan mengelola informasi mahasiswa, seperti data pribadi, program studi, nilai akademik, dan status kelulusan. Dengan antarmuka yang sederhana dan mudah digunakan, aplikasi ini membantu proses pencatatan data menjadi lebih cepat, terstruktur, dan efisien.

---
+++ AplikasiInputDataMahasiswa/AplikasiInputDataMahasiswa/Mahasiswa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiInputDataMahasiswa
+{
+    class Mahasiswa
+    {
+        public string Nim { get; internal set; }
+        public string Nama { get; internal set; }
+        public string Kelas { get; internal set; }
+        public int Nilai { get; internal set; }
+    }
+}
